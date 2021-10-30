c9824b3f42cd9751f4f0894fa8b18b798832c1ed
hiagoLF <hiagolof@gmail.com>
2021-10-29 23:56:33 -0300

    Revert "Revert "Credits and PCR pages and links added""
    This reverts commit 50f1f2297f45f4df68f42c331b22ecdd45d5424e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import PageHeader from "./components/PageHeader";
 import HomePage from "./pages/HomePage";
 import PrimersPage from "./pages/PrimersPage";
-import './index.css'
+import "./index.css";
+import CreditsPage from "./pages/CreditsPage";
+import LearnPCR from "./pages/LearnPCR";
 
 function App() {
-   return (
-      <div className="App">
-         <PageHeader />
-         <Router>
-            <Switch>
-               <Route path="/" exact component={HomePage} />
-               <Route path="/primers" component={PrimersPage} />
-            </Switch>
-         </Router>
-      </div>
-   );
+  return (
+    <div className="App">
+      <PageHeader />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/primers" component={PrimersPage} />
+          <Route path="/about-pcr" component={LearnPCR} />
+          <Route path="/credits" exact component={CreditsPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/CreditsPage/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const CreditsPageContainer = styled.div`
+  flex: 1;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+
+  #profile-container {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    margin-bottom: 10px;
+
+    hr {
+      height: 3px;
+      color: #000;
+    }
+
+    img {
+      height: 200px;
+      width: 200px;
+      border-radius: 10px;
+      margin-bottom: 10px;
+    }
+
+    h2 {
+    }
+    h3 {
+      margin-bottom: 10px;
+    }
+    h4 {
+      opacity: 0.8;
+    }
+  }
+`;
